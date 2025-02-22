43e0c77249160fe247d874da2eb9d9e09d36793b
Gigi <lruiz@edu.uag.mx>
2025-02-21 18:43:02 -0600

    Add files via upload

---
+++ Nodos.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace torres_de_hanoi_100__real_no_fake
+{
+    internal class nodo
+    {
+        public nodo next;
+        public nodo prev;
+        public int dato;
+
+        public nodo(int dato)
+        {
+            this.dato = dato;
+            this.prev = null;
+            this.next = null;
+        }
+
+        public nodo(int dato, nodo prev)
+        {
+            this.dato = dato;
+            this.prev = prev;
+            this.next = null; 
+        }
+
+        public nodo (int dato, nodo prev, nodo next)
+        {
+            this.dato = dato;
+            this.prev = prev;
+            this.next = next; 
+        }
+
+
+    }
+}
---
+++ pilas.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace torres_de_hanoi_100__real_no_fake
+{
+    internal class pila
+    {
+        public nodo primero;
+
+        public bool pilavacia()
+        {
+            return primero == null;
+        }
+
+        public void push(int valor)
+        {
+            nodo chepusei = new nodo(valor);
+            if (pilavacia())
+            {
+                primero = chepusei;
+                return;
+            }
+            chepusei.next = primero;
+            primero.prev = chepusei;
+            primero = chepusei;
+        }
+
+        public int pop()
+        {
+            if (pilavacia())
+            {
+                return 0;
+            }
+            int valor = primero.dato;
+            primero = primero.next;
+            if (primero != null)
+            {
+                primero.prev = null;
+            }
+            return valor;
+        }
+
+        public int peek()
+        {
+            if (pilavacia())
+            {
+                return 0;
+            }
+            return primero.dato;
+        }
+
+    }
+}
